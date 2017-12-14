fa65bff4eaf9e781fe5b169986016badfd43c068
ChristopherJamesN <nady.christopher@gmail.com>
2017-12-14 09:42:04 -0600

    Add routes and scope to workout model for week and todays workouts

--- app/models/workout.rb
+++ app/models/workout.rb
@@ -1,4 +1,15 @@
 class Workout < ApplicationRecord
   validates :type, :calories, :duration, :presence => true
   belongs_to :user
+
+  scope :this_weeks_workouts, -> { where("DATE(created_at) > ?", (Date.today).to_time - 7.days) }
+  scope :todays_workouts, -> { where("DATE(created_at) > ?", (Date.today).to_time - 1.days) }
+
+  def self.total_calories
+    calories = 0
+    Workout.all.each do |workout|
+      calories+= workout.calories
+    end
+    calories
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   devise_for :users
+  get '/workouts/today', to: 'workouts#today'
+  get '/workouts/week', to: 'workouts#week'
   resources :workouts
   root to: "workouts#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
